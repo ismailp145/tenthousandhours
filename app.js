238600b9b99cd52e82ae528afa4d7ed8f2de6962
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-10-31 11:58:54 -0500

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+function timer() {
+  var sec = 30;
+  var timer = setInterval(function () {
+    document.getElementById("safeTimerDisplay").innerHTML = "00:" + sec;
+    sec--;
+    if (sec < 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+element.addEventListener("click", function () {
+  alert("Hello World!");
+});
