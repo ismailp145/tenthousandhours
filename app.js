3e1b1ec2c65cc0e71e43b3b7e79dea23a43adcfc
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-11-02 10:49:50 -0600

    some progress on start timer.

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-function timer() {
-  var sec = 30;
-  var timer = setInterval(function () {
-    document.getElementById("safeTimerDisplay").innerHTML = "00:" + sec;
-    sec--;
-    if (sec < 0) {
-      clearInterval(timer);
-    }
+const playButton = document.getElementById("play-button");
+const stopButton = document.getElementById("stop-button");
+const resetButton = document.getElementById("reset-button");
+const safeTimer = document.getElementById("timer");
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+playButton.addEventListener("click", () => {
+  console.log("playbuttonclicked");
+  startTimer();
+});
+
+stopButton.addEventListener("click", () => {
+  console.log("playbuttonclicked");
+  stopTimer();
+});
+
+function startTimer() {
+  var timer = setInterval(() => {
+    safeTimer.innerHTML = "00:00:" + seconds;
+    seconds++;
   }, 1000);
 }
 
-element.addEventListener("click", function () {
-  alert("Hello World!");
-});
+function stopTimer() {}
