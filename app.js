4bb83cbeee16e4efd2830bdd8336c30494ea23d5
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-11-02 13:13:01 -0600

    isRunning boolean added to avoid adding additional intervals

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const safeTimer = document.getElementById("timer");
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+let isRunning = false;
+const TARGET_HOURS = 10000;
+const TARGET_SECONDS = TARGET_HOURS * 3600;
 
 playButton.addEventListener("click", () => {
   console.log("playbuttonclicked");
@@ -13,15 +16,22 @@ playButton.addEventListener("click", () => {
 });
 
 stopButton.addEventListener("click", () => {
-  console.log("playbuttonclicked");
+  console.log("stopbuttonclicked");
   stopTimer();
 });
 
 function startTimer() {
-  var timer = setInterval(() => {
-    safeTimer.innerHTML = "00:00:" + seconds;
-    seconds++;
-  }, 1000);
+  if (!isRunning) {
+    console.log(isRunning);
+    let timer = setInterval(() => {
+      safeTimer.innerHTML = "00000:00:" + seconds;
+      seconds++;
+    }, 1000);
+    isRunning = true;
+  }
 }
 
-function stopTimer() {}
+function stopTimer() {
+  clearInterval(timer);
+  isRunning = false;
+}
